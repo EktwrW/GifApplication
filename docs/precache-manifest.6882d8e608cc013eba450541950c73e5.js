66ed311ba1e1b81628ea8c481f9af764a3b4b3bb
Hector <hwaldmann@gmail.com>
2020-08-25 21:43:22 -0300

    Buscador de Gifs
    Motorizado con API Giphy

---
+++ docs/precache-manifest.6882d8e608cc013eba450541950c73e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c22a4efcef86bd4429086804d418b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2d769031655f012d33",
+    "url": "/static/css/main.1846e778.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2d769031655f012d33",
+    "url": "/static/js/main.535f309d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
